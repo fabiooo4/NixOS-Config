a841ac276a0b6d7f329ea0fa4a2c9be6ac9e3389
fabiooo4 <fabioirimie@gmail.com>
2025-01-26 00:30:04 +0100

    110 current  2025-01-25 23:54:49  25.05.20250123.0aa4755     6.12.10                          *

--- home-manager/dotfiles.nix
+++ home-manager/dotfiles.nix
@@ -93,5 +93,12 @@ in {
         source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/.config/nvim";
       };
     };
+
+    # Starship
+    home.file = {
+      ".config/starship.toml" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/.config/starship.toml";
+      };
+    };
   };
 }
