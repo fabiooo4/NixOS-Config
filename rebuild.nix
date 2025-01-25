91a28820d534dd4e3675fe2d96d47ede092f7765
fabiooo4 <fabioirimie@gmail.com>
2025-01-25 15:27:08 +0100

    93 current  2025-01-25 15:26:43  25.05.20250121.9e4d519     6.12.10                          *

--- rebuild.nix
+++ rebuild.nix
@@ -29,7 +29,7 @@ pkgs.writeShellScriptBin "rebuild" ''
   echo "NixOS Rebuilding..."
 
   # Rebuild, output simplified errors and progress, log tracebacks
-  sudo nixos-rebuild switch --flake ${nixosDirectory} &> nixos-switch.log | (tail -f nixos-switch.log | grep "built\|copied\|MiB") || (cat nixos-switch.log | grep --color error && exit 1)
+  sudo nixos-rebuild switch --flake ${nixosDirectory} &> nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
 
   # Get current generation metadata
   current=$(nixos-rebuild list-generations | grep current)
