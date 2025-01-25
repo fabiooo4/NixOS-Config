bbdfc9fde5bcad5ce1e779c19f7bb6799899db95
fabiooo4 <fabioirimie@gmail.com>
2025-01-25 12:20:05 +0100

    first commit

---
+++ configuration.nix
@@ -0,0 +1,176 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+      inputs.xremap-flake.nixosModules.default
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Rome";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "it_IT.UTF-8";
+    LC_IDENTIFICATION = "it_IT.UTF-8";
+    LC_MEASUREMENT = "it_IT.UTF-8";
+    LC_MONETARY = "it_IT.UTF-8";
+    LC_NAME = "it_IT.UTF-8";
+    LC_NUMERIC = "it_IT.UTF-8";
+    LC_PAPER = "it_IT.UTF-8";
+    LC_TELEPHONE = "it_IT.UTF-8";
+    LC_TIME = "it_IT.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.core-utilities.enable = false;
+
+  # Remove default programs
+  services.xserver.excludePackages = [ pkgs.xterm ];
+  environment.gnome.excludePackages = [ pkgs.gnome-tour ];
+  environment.extraSetup = ''
+    rm $out/share/applications/cups.desktop
+  '';
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "intl";
+  };
+
+  # Configure remaps
+  services.xremap = {
+    userName = "fabibo";
+    config = {
+      keymap = [
+        {
+          name = "Capslock to Esc";
+	  remap = {
+            "CapsLock" = "esc";
+	  };
+	}
+      ];
+    };
+  };
+
+  # Configure console keymap
+  console.keyMap = "us-acentos";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.fabibo = {
+    isNormalUser = true;
+    description = "Fabio";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    users = {
+      "fabibo" = import ./home.nix;
+    };
+  };
+
+  # Enable automatic login for the user.
+  # services.xserver.displayManager.autoLogin.enable = true;
+  # services.xserver.displayManager.autoLogin.user = "fabibo";
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Activate flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    google-chrome
+    sioyek
+    kitty
+    zoxide
+    fzf
+    eza
+    bat
+  ];
+
+  # Change default shell to zsh
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "24.11"; # Don't change
+}
---
+++ home/dotfiles.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, lib, ... }:
+{
+  options = {
+    dotfiles = lib.mkOption {
+      type = lib.types.path;
+      apply = toString;
+      default = "${config.home.homeDirectory}/.dotfiles";
+      example = "${config.home.homeDirectory}/.dotfiles";
+      description = "Location of the dotfiles";
+    };
+  };
+  config = {
+    home.activation = {
+      # Clone dotfiles repo
+      cloneDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
+	  if ! [[ -d "${config.dotfiles}" ]]; then
+            ${pkgs.git}/bin/git clone https://github.com/fabiooo4/dotfiles.git ${config.dotfiles} 
+            ${pkgs.git}/bin/git clone https://github.com/fabiooo4/Neovim.git ${config.dotfiles}/.config/nvim
+            cd ${config.dotfiles}
+            ${pkgs.git}/bin/git remote remove origin
+            ${pkgs.git}/bin/git remote add origin git@github.com:fabiooo4/dotfiles.git
+            cd ${config.dotfiles}/.config/nvim
+            ${pkgs.git}/bin/git remote remove origin
+            ${pkgs.git}/bin/git remote add origin git@github.com:fabiooo4/Neovim.git
+          fi
+        '';
+    };
+
+    # Kitty
+    home.file = {
+     ".config/kitty" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/.config/kitty";
+      };
+    };
+
+    # Zsh
+    home.activation = {
+      # Clone zplug for plugin management
+      # This is needed because all the plugins are in .zshrc
+      cloneZplug = lib.hm.dag.entryAfter ["writeBoundary"] ''
+	  if ! [[ -d "$HOME/.zplug" ]]; then
+            ${pkgs.git}/bin/git clone https://github.com/zplug/zplug ~/.zplug
+          fi
+        '';
+    };
+    home.file = {
+     ".zshrc" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/.zshrc";
+      };
+
+     ".zsh" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/.zsh";
+      };
+    };
+
+    # Latexmkrc
+    home.file = {
+     ".latexmkrc" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/.latexmkrc";
+      };
+    };
+
+    # Gitconfig
+    home.file = {
+     ".gitconfig" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/.gitconfig";
+      };
+    };
+
+    # Sioyek
+    home.file = {
+     ".config/sioyek" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/.config/sioyek";
+      };
+    };
+
+    # Nvim
+    home.file = {
+     ".config/nvim" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/.config/nvim";
+      };
+    };
+  };
+}
