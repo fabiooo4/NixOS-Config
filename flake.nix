88b51b491a7fc6a7392dd996df8fcec8eb67dc73
fabiooo4 <fabioirimie@gmail.com>
2025-01-25 16:02:38 +0100

    96 current  2025-01-25 16:02:33  25.05.20250121.9e4d519     6.12.10                          *

--- flake.nix
+++ flake.nix
@@ -15,14 +15,24 @@
   outputs = {
     self,
     nixpkgs,
+    home-manager,
     ...
-  } @ inputs: {
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
         inputs.home-manager.nixosModules.default
       ];
+    };
+
+    homeConfigurations.nixos = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      modules = [./home.nix];
     };
   };
 }
