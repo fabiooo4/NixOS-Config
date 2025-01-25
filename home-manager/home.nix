6f297fb5e65d4387cf96ddfe5191d43bbc31a135
fabiooo4 <fabioirimie@gmail.com>
2025-01-25 20:47:18 +0100

    101 current  2025-01-25 20:12:38  25.05.20250123.0aa4755     6.12.10                          *

---
+++ home-manager/home.nix
@@ -0,0 +1,63 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [./dotfiles.nix];
+  nixpkgs.config.allowUnfree = true;
+
+  home.username = "fabibo";
+  home.homeDirectory = "/home/fabibo";
+
+  home.stateVersion = "24.11"; # Don't change
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment
+  home.packages = with pkgs; [
+    starship
+    # Neovim dependencies
+    lua51Packages.lua
+    luajitPackages.luarocks
+    stylua
+    nixd
+    alejandra
+    nodePackages.prettier
+    ripgrep
+    gcc
+    nodejs
+    xclip
+    fd
+    unzip
+    wget
+    tree-sitter
+    texliveFull
+    gnumake
+    rustup
+    # --------------
+  ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraLuaPackages = ps: [ps.magick];
+    extraPackages = [pkgs.imagemagick];
+  };
+
+  # Setup rustup
+  home.activation.rustupDefaultChannel = ''
+    run ${pkgs.rustup}/bin/rustup default stable
+    run ${pkgs.rustup}/bin/rustup component add rust-analyzer
+  '';
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    BROWSER = "google-chrome";
+    TERMINAL = "kitty";
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # Let Home Manager install and manage itself
+  programs.home-manager.enable = true;
+}
