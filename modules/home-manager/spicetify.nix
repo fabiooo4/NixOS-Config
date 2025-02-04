c262b35256aea6f1b75f0ad3f578f530126e892e
fabiooo4 <fabioirimie@gmail.com>
2025-02-04 16:39:22 +0100

    123 current  2025-02-04 16:02:35  25.05.20250123.0aa4755     6.12.10                          *

--- modules/home-manager/spicetify.nix
+++ modules/home-manager/spicetify.nix
@@ -1,10 +1,24 @@
-{inputs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+in {
   imports = [
     inputs.spicetify-nix.homeManagerModules.spicetify
   ];
 
+  stylix.targets.spicetify.enable = false;
+
   programs.spicetify = {
     enable = true;
-    #config options
+    enabledExtensions = with spicePkgs.extensions; [
+      adblockify
+      hidePodcasts
+      shuffle # shuffle+ (special characters are sanitized out of extension names)
+    ];
+    theme = spicePkgs.themes.comfy;
+    # colorScheme = "mocha";
   };
 }
